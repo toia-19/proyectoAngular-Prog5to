75e0f0b6a068d6fd6e0dcc0428639aceaf937fdf
toia-19 <victoriaangelinadiaz@gmail.com>
2025-09-23 15:28:24 -0300

    Creación de primeros componentes

---
+++ src/app/componentes/aside/aside.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'aside',
+  imports: [],
+  templateUrl: './aside.html',
+  styleUrl: './aside.css'
+})
+export class Aside {
+
+}
---
+++ src/app/componentes/main/main.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main',
+  imports: [],
+  templateUrl: './main.html',
+  styleUrl: './main.css'
+})
+export class Main {
+
+}
