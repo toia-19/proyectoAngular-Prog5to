447a1926e51e0f32fe07ef43a30ee04a8064a780
toia-19 <victoriaangelinadiaz@gmail.com>
2025-10-30 16:49:44 -0300

    Repaso de responsividad; creación de primera interfaz y uso de directiva 'ngFor'

---
+++ src/app/models/tarjeta.ts
@@ -0,0 +1,23 @@
+/* 
+    ng g i nombreInterfaz
+    ng generate interface nombreInterfaz
+
+    Interfaz: Modelo de clases u objetos, donde se definen las propiedades 
+    (características) y que nos permiten estructurarlas con sus tipos
+
+    TIPOS:
+        - string: alfanuméricos
+        - number: numéricos
+        - boolean: booleanos
+        - Date: tipo de fecha
+*/
+
+export interface Tarjeta {
+    id: number;
+    titulo: string; // string <- valores alfanumericos (letras y números)
+    descripcion: string;
+    precio: number; // number <- valores numéricos
+    fechaPostal: Date;
+    img: string;
+    alt: string;
+}
--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -1,11 +1,67 @@
 import { Component } from '@angular/core';
+import { Tarjeta } from '../../models/tarjeta';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './products.html',
   styleUrl: './products.css'
 })
 export class Products {
+  /*
+    Propiedades en Angular -> variables
+    Propiedad PÚBLICA: libre acceso a su contenido
+    Propiedad PRIVADA: contenido restringido
+  */
+  public infoTarjeta: Tarjeta[];
 
+  /* 
+    Instanciar a la propiedad para poder hacer uso de ella
+
+    Declaramos colección de tarjetas en base a modelo de interfaz "Tarjetas"
+    - Se tiene que respetar el uso de todas las propiedades y su tipo
+    - Tipo alfanumérico (string) inicializa en ""
+    - Tipo numérico (number) inicializa en 0
+  */
+  constructor(){
+    this.infoTarjeta = [
+      {
+        id: 1,
+        titulo: "Border Collie",
+        descripcion: "Foto de un border collie",
+        precio: 2000,
+        fechaPostal: new Date('2020-12-1'),
+        img: "https://images.pexels.com/photos/28645781/pexels-photo-28645781.jpeg",
+        alt: "Postal de un border collie",
+      },
+      {
+        id: 2,
+        titulo: "Golden Retriever",
+        descripcion: "Foto de dos goldens",
+        precio: 3500,
+        fechaPostal: new Date('2025-12-1'),
+        img: "../../../assets/pexels-chevanon-1108099.jpg",
+        alt: "Postal de dos goldens"
+      },
+      {
+        id: 3,
+        titulo: "Pastor Aleman",
+        descripcion: "Foto de pastor aleman",
+        precio: 1500,
+        fechaPostal: new Date('2024-8-12'),
+        img: "https://images.pexels.com/photos/333083/pexels-photo-333083.jpeg",
+        alt: "Postal de pastor aleman"
+      },
+      {
+        id: 4,
+        titulo: "Perrito bebé",
+        descripcion: "Foto de cachorro",
+        precio: 5000,
+        fechaPostal: new Date('2022-8-11'),
+        img: "https://images.pexels.com/photos/59965/dog-young-dog-puppy-59965.jpeg",
+        alt: "Postal de cachorro"
+      },
+    ]
+  }
 }
